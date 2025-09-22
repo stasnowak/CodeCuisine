cfee8a7c13564d3a19b3a72e07ba4edbdb238b18
snwk <stas@nwkmail.com>
2025-09-22 19:59:54 +0200

    chore: remove redundant comment from GlobalUsings.cs

--- CodeCuisine/GlobalUsings.cs
+++ CodeCuisine/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿// Global using directives
-
-global using System.Text;
+﻿global using System.Text;
 global using CodeCuisine.Brokers;
 global using CodeCuisine.Options;
 global using CommandDotNet;
